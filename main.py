4c7ae42d89cea043eefe2b7cc222b7ccc9139800
Glukhareva <114655236+Glukhareva@users.noreply.github.com>
2022-10-21 12:31:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pandas as pd
 data = pd.read_csv("test_3.csv", header = None)
-for i in range(len(data)):
-    if data[i] == data.max():
-        data[i].append(1)
-    else:
-        data[i].append(0)
-print(data)
+data1 = data.to_numpy()
+for i in range(len(data1)-1):
+    for j in range(len(data1[i])):
+        if data1[i][j] >= data1[i + 1][j]:
+            np.append(data1[i], 0)
+        else: 
+            np.append(data1[i], 1)
+print(data1)
